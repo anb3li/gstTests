7860bc7a73551ea0f5e25b527afea1e3bb1e795f
anb3li <anbeli260907@gmail.com>
2024-09-27 20:10:20 +0200

    comiiit

--- main.c
+++ main.c
@@ -11,8 +11,14 @@ int main(int argc, char *argv[]) {
 
     // Create pipeline using gst_parse_launch
     pipeline = gst_parse_launch(
-        "souphttpsrc location=https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg ! "
-        "rsvgdec ! videoconvert ! autovideosink",
+        "\
+   videotestsrc pattern=1 ! \
+   video/x-raw,format=AYUV,framerate=\(fraction\)10/1,width=100,height=100 ! \
+   videobox border-alpha=0 top=-70 bottom=-70 right=-220 ! \
+   compositor name=comp sink_0::alpha=0.7 sink_1::alpha=0.5 ! \
+   videoconvert ! xvimagesink \
+   videotestsrc ! \
+   video/x-raw,format=AYUV,framerate=\(fraction\)5/1,width=320,height=240 ! comp.",
         NULL);
 
     // Start playing the pipeline
