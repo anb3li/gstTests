5e1b0b719f58d9b5bb483be035acf5fd27901431
anb3li <anbeli260907@gmail.com>
2024-09-27 19:20:26 +0200

    ja

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-    GstElement *pipeline, *selector, *sink_pad_1, *sink_pad_2;
+    GstElement *pipeline;
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
@@ -11,12 +11,9 @@ int main(int argc, char *argv[]) {
 
     // Create pipeline using gst_parse_launch
     pipeline = gst_parse_launch(
-        "videotestsrc pattern=0 ! video/x-raw,width=640,height=480 ! inputselector name=selector ! autovideosink "
-        "videotestsrc pattern=1 ! video/x-raw,width=640,height=480 ! selector.sink_1", 
+        "souphttpsrc location=https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg ! "
+        "rsvgdec ! videoconvert ! autovideosink",
         NULL);
-
-    // Get the inputselector element by name
-    selector = gst_bin_get_by_name(GST_BIN(pipeline), "selector");
 
     // Start playing the pipeline
     ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
@@ -26,24 +23,15 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    // Retrieve pads for switching
-    sink_pad_1 = gst_element_get_static_pad(selector, "sink_1");
-
-    // Sleep for 5 seconds to display the first source
-    g_print("Displaying first source for 5 seconds...\n");
-    g_usleep(5 * G_USEC_PER_SEC);
-
-    // Switch to the second source (videotestsrc pattern=1)
-    g_print("Switching to second source...\n");
-    g_object_set(selector, "active-pad", sink_pad_1, NULL);
-
-    // Sleep for another 5 seconds to display the second source
-    g_usleep(5 * G_USEC_PER_SEC);
+    // Wait until error or EOS (End of Stream)
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
     // Free resources
+    if (msg != NULL)
+        gst_message_unref(msg);
+    gst_object_unref(bus);
     gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(sink_pad_1);
-    gst_object_unref(selector);
     gst_object_unref(pipeline);
 
     return 0;
